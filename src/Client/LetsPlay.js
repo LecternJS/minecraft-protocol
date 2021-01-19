1495033773743b864c196746ed7ea948a1609ad2
Jordan Jones <jordpjones@gmail.com>
2021-01-19 11:54:13 -0800

    Move disconnect listener

--- src/Client/LetsPlay.js
+++ src/Client/LetsPlay.js
@@ -37,6 +37,26 @@ class LetsPlay {
 		});
 	}
 
+	disconnect(message) {
+		if (!message.reason) return;
+		message = JSON.parse(message.reason);
+		let text = message.text ? message.text : message;
+		let versionRequired;
+
+		if (text.translate && text.translate.startsWith('multiplayer.disconnect.outdated_')) {
+			[versionRequired] = text.with;
+		} else {
+			if (text.extra) [[text]] = text.extra;
+			versionRequired = /(?:Outdated client! Please use|Outdated server! I'm still on) (.+)/.exec(text);
+			versionRequired = versionRequired ? versionRequired[1] : null;
+		}
+
+		if (!versionRequired) { return; }
+		this.end();
+		this.emit('error', new Error(`This server is version ${versionRequired
+		}, you are using version ${this.version}, please specify the correct version in the options.`));
+	}
+
 }
 
 module.exports = LetsPlay;
