37e6ed36eff3c60f179a4daff10959f5cd4a5262
Jordan Jones <jordpjones@gmail.com>
2021-06-23 04:40:05 -0700

    Fix missing states/issues with serializer

--- src/Util/Serializer.js
+++ src/Util/Serializer.js
@@ -3,7 +3,7 @@ const { Serializer } = require('protodef');
 const manageProtocols = require('./ProtocolHandler');
 const Constants = require('./Constants');
 
-function createSerializer(state = Constants.States.Handshaking, isServer = false, version = Constants.DefaultMinecraftVersion, customPackets = {}, compiled = true) {
+function createSerializer({ state = Constants.States.Handshaking, isServer = false, version, customPackets, compiled = true } = {}) {
 	return new Serializer(manageProtocols(state, !isServer ? 'toServer' : 'toClient', version, customPackets, compiled), 'packet');
 }
 
